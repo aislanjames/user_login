88903ed0aabd1c7b8c57bc4d753e07c62c071240
aislanjames <aislanjames@gmail.com>
2024-05-15 12:47:17 -0300

    Criação da estrutura

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center mt-5">
+      <h1 className="text-4xl font-bold text-blue-600">Bem-vindo ao React com Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
+
